a3a0cc55e1b1fe113626abea8837198995f268dc
Brandon Morales <brandonjmm11@gmail.com>
2025-05-21 15:35:45 -0400

    Didasko onli

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express =  require('express');
 const app = express();
 const mime = require('mime-types');
 
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.set("view engine", 'ejs');
 
 //app.use(express.static('public'));
